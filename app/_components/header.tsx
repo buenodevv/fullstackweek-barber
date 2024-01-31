86ef6cc49665a184415d74b796a04604c06444b4
buenodevv <buenodevv@gmail.com>
2024-01-31 16:54:36 -0300

    feat: head ok

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,12 +1,20 @@
 import Image from "next/image";
-import { Card } from "./ui/card";
+import { Card, CardContent } from "./ui/card";
+import { Button } from "./ui/button";
+import { MenuIcon } from "lucide-react";
 
 const Header = () => {
-    return ( 
-         <Card>
-            <Image src="/logo.png" alt="FSW Barber" height={22} width={120}/>
-         </Card>
-     );
+    return (
+        <Card>
+            <CardContent className="px-5 py-8 justify-between flex flex-row">
+                <Image src="/logo.png" alt="FSW Barber" height={18} width={120} />
+                <Button variant="outline" size="icon" className="h-8 w-8">
+                    <MenuIcon size={18}/>
+                </Button>
+            </CardContent>
+
+        </Card>
+    );
 }
- 
+
 export default Header;
